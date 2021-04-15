2469079f7c098e9ecf9f6f44530cb2c2641a83ff
Cameron Baney <cbaney@gmail.com>
2021-04-15 16:51:11 -0400

    feat(Documentation): Replace help links with Pennsieve Docs

--- src/components/datasets/DatasetPermissions/DatasetPermissions.vue
+++ src/components/datasets/DatasetPermissions/DatasetPermissions.vue
@@ -31,7 +31,7 @@
       <p>
         Give teams, individuals, or your entire organization access to your dataset within the Pennsieve platform.
         <a
-          href="http://help.blackfynn.com/blackfynn-web-application/interacting-with-other-users-on-the-platform/dataset-permissions"
+          href="https://docs.pennsieve.io/docs/dataset-permissions"
           target="_blank"
         >
           Learn More
