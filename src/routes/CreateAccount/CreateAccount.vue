9d7718f01f292e7d30420b263df4ebe0185cc76b
Cameron Baney <cbaney@gmail.com>
2021-04-27 10:00:08 -0400

    Formatting

--- src/routes/CreateAccount/CreateAccount.vue
+++ src/routes/CreateAccount/CreateAccount.vue
@@ -2,36 +2,58 @@
   <div class="wrapper">
     <div class="create-account-wrap">
       <div v-if="!accountCreated">
-        <h2 class="sharp-sans">Create Your Account</h2>
-      <p>
-        Welcome to Pennsieve! Complete the following form so that we can
-        register your account.
-        <strong>All fields are required</strong>.
-      </p>
-      <el-form
-        id="signup-form"
-        ref="signupForm"
-        :model="signupForm"
-        :rules="signupRules"
-        status-icon
-        @submit.native.prevent="onFormSubmit"
-      >
-        <el-form-item label="First Name" prop="firstName">
-          <el-input
-            v-model="signupForm.firstName"
-            required
-            class="first-name-input"
-            autofocus
-          />
-        </el-form-item>
-        <el-form-item label="Last Name" prop="lastName">
-          <el-input v-model="signupForm.lastName" required />
-        </el-form-item>
-        <el-form-item label="Email" prop="email">
-          <el-input v-model="signupForm.email" required />
-        </el-form-item>
-        <el-form-item>
-            <bf-button class="secondary" @click="onFormCancel">Cancel</bf-button>
+        <h2 class="sharp-sans">
+          Create Your Account
+        </h2>
+        <p>
+          Welcome to Pennsieve! Complete the following form so that we can
+          register your account.
+          <strong>All fields are required</strong>.
+        </p>
+        <el-form
+          id="signup-form"
+          ref="signupForm"
+          :model="signupForm"
+          :rules="signupRules"
+          status-icon
+          @submit.native.prevent="onFormSubmit"
+        >
+          <el-form-item
+            label="First Name"
+            prop="firstName"
+          >
+            <el-input
+              v-model="signupForm.firstName"
+              required
+              class="first-name-input"
+              autofocus
+            />
+          </el-form-item>
+          <el-form-item
+            label="Last Name"
+            prop="lastName"
+          >
+            <el-input
+              v-model="signupForm.lastName"
+              required
+            />
+          </el-form-item>
+          <el-form-item
+            label="Email"
+            prop="email"
+          >
+            <el-input
+              v-model="signupForm.email"
+              required
+            />
+          </el-form-item>
+          <el-form-item>
+            <bf-button
+              class="secondary"
+              @click="onFormCancel"
+            >
+              Cancel
+            </bf-button>
             <bf-button
               :processing="isCreatingAccount"
               processing-text="Submitting"
@@ -39,23 +61,33 @@
             >
               Create Account
             </bf-button>
-        </el-form-item>
-      </el-form>
-      <p class="agreement">
-        By clicking “Create Account" you are agreeing to the Pennsieve
-        <a href="https://www.blackfynn.com/terms" target="_blank">
-          Terms of Use
-        </a>
-        and
-        <a href="https://www.blackfynn.com/privacy" target="_blank">
-          Privacy Policy
-        </a>.
-      </p>
+          </el-form-item>
+        </el-form>
+        <p class="agreement">
+          By clicking “Create Account" you are agreeing to the Pennsieve
+          <a
+            href="https://www.blackfynn.com/terms"
+            target="_blank"
+          >
+            Terms of Use
+          </a>
+          and
+          <a
+            href="https://www.blackfynn.com/privacy"
+            target="_blank"
+          >
+            Privacy Policy
+          </a>.
+        </p>
       </div>
       <div v-else>
-         <h2 class="sharp-sans">Thank You</h2>
-         <p>Thank you for registering an account. An email should have been sent to create your password.</p>
-        <router-link :to="{name: 'login'}">Back to login</router-link>
+        <h2 class="sharp-sans">
+          Thank You
+        </h2>
+        <p>Thank you for registering an account. An email should have been sent to create your password.</p>
+        <router-link :to="{name: 'login'}">
+          Back to login
+        </router-link>
       </div>
     </div>
   </div>
@@ -63,46 +95,47 @@
 
 <script>
 import BfButton from '@/components/shared/bf-button/BfButton.vue'
-  export default {
-    name: 'CreateAccount',
-    components: {
-      BfButton,
-    },
-    data() {
-      return {
-        isCreatingAccount: false,
-        accountCreated: false,
-        signupForm: {
-        firstName: '',
-        lastName: '',
-        email: '',
-      },
-      profileRules: {
-        firstName: [
-          { required: true, message: 'Please enter your first name', trigger: 'submit' }
-        ],
-        lastName: [
-          { required: true, message: 'Please enter your last name', trigger: 'submit' }
-        ],
-        jobTitle: [
-          { required: true, message: 'Please enter your email', trigger: 'submit' }
-        ]
-      },
-      }
-    },
-    methods: {
-      onFormCancel: function() {
-        this.$router.push({
-          name: 'login'
-        })
-      },
 
-      onFormSubmit: function() {
-        this.isCreatingAccount = true
-        this.accountCreated = true
-      },
+export default {
+  name: 'CreateAccount',
+  components: {
+    BfButton,
+  },
+  data() {
+    return {
+      isCreatingAccount: false,
+      accountCreated: false,
+      signupForm: {
+      firstName: '',
+      lastName: '',
+      email: '',
     },
-  }
+    signupRules: {
+      firstName: [
+        { required: true, message: 'Please enter your first name', trigger: 'submit' }
+      ],
+      lastName: [
+        { required: true, message: 'Please enter your last name', trigger: 'submit' }
+      ],
+      jobTitle: [
+        { required: true, message: 'Please enter your email', trigger: 'submit' }
+      ]
+    },
+    }
+  },
+  methods: {
+    onFormCancel: function() {
+      this.$router.push({
+        name: 'login'
+      })
+    },
+
+    onFormSubmit: function() {
+      this.isCreatingAccount = true
+      this.accountCreated = true
+    },
+  },
+}
 </script>
 
 <style lang="scss" scoped>
