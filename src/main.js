4ca448bbb8be4994026773052f414edb115b679e
Annalee Switek <annaleeswitek@Annalees-MBP.hsd1.pa.comcast.net>
2023-05-26 14:06:20 -0400

    added route name to allowList

--- src/main.js
+++ src/main.js
@@ -178,9 +178,12 @@ const router = new VueRouter({
 sync(store, router)
 
 const isAuthorized = (to, from, next) => {
+  console.log('to', to)
+  console.log('from', from)
+  console.log('next', next)
   const token = Cookies.get('user_token')
   const savedOrgId = Cookies.get('preferred_org_id')
-  const allowList = ['home', 'password', 'welcome', 'setup-profile', 'verify-account','welcome-to-pennsieve', 'docs-login', 'jupyter-login','create-account']
+  const allowList = ['home', 'password', 'welcome', 'setup-profile', 'setup-profile-accept', 'verify-account','welcome-to-pennsieve', 'docs-login', 'jupyter-login','create-account']
   if (allowList.indexOf(to.name) < 0 && !token) {
     const destination = to.fullPath
     if (destination && destination.name !== 'page-not-found') {
