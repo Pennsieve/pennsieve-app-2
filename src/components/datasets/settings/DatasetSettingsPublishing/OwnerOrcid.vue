18a34c47b09ca187e3884b0761651742997204bc
Annalee Switek <annaleeswitek@Annalees-MacBook-Pro.local>
2023-08-08 15:06:30 -0400

    updated logic to only show the Connect your Orchid iD ui element to dataset owners with no connected orcid id

--- src/components/datasets/settings/DatasetSettingsPublishing/OwnerOrcid.vue
+++ src/components/datasets/settings/DatasetSettingsPublishing/OwnerOrcid.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="owner-orcid">
-    <div v-if="!datasetOwnerHasOrcidId || !hasOrcidId">
+    <div v-if="!hasOrcidId">
       <p class="sharing-blurb">
-        Connect your ORCIDiD to your Pennsieve account to enable publishing.
+        Connect your ORCID iD to your Pennsieve account to enable publishing.
         <a
           href="https://docs.pennsieve.io/docs/orcid-ids-on-the-pennsieve-platform"
           target="_blank"
@@ -13,7 +13,10 @@
       </p>
       <router-link
         class="bf-menu-item"
-        :to="{ name: 'my-settings-container', params: {orgId: this.activeOrganizationId} }"
+        :to="{
+          name: 'my-settings-container',
+          params: { orgId: this.activeOrganizationId }
+        }"
       >
         Connect ORCID iD in user profile menu
       </router-link>
@@ -22,18 +25,16 @@
     <div v-else>
       <div>
         <p class="orcid-success-text blurb">
-          Below is the ORCID associated with your Pennsieve account. <a
+          Below is the ORCID associated with your Pennsieve account.
+          <a
             href="https://docs.pennsieve.io/docs/orcid-ids-on-the-pennsieve-platform"
             target="_blank"
           >
             Learn More
           </a>
         </p>
-        <div
-          v-if="!loadingOrcid"
-          class="orcid-success"
-        >
-          <img src="/static/images/orcid.png">
+        <div v-if="!loadingOrcid" class="orcid-success">
+          <img src="/static/images/orcid.png" />
           <el-row
             class="orcid-success-info"
             type="flex"
@@ -41,34 +42,19 @@
             alt="Logo for ORCID"
           >
             <el-col>
-              <a
-                :href="getORCIDResultUrl"
-                target="_blank"
-              >
+              <a :href="getORCIDResultUrl" target="_blank">
                 {{ getORCIDResultUrl }}
               </a>
             </el-col>
           </el-row>
           <el-col class="orcid-delete-button">
             <button @click="openORCIDWindow">
-              <svg-icon
-                icon="icon-remove"
-                height="10"
-                width="10"
-              />
+              <svg-icon icon="icon-remove" height="10" width="10" />
             </button>
           </el-col>
         </div>
-        <div
-          v-else
-          class="orcid-waiting"
-        >
-          <el-row>
-            <div
-              v-loading="loadingOrcid"
-              class="orcid-loader"
-            />
-          </el-row>
+        <div v-else class="orcid-waiting">
+          <el-row><div v-loading="loadingOrcid" class="orcid-loader" /></el-row>
         </div>
       </div>
     </div>
@@ -101,23 +87,20 @@ export default {
   },
 
   computed: {
-    ...mapGetters([
-      'datasetOwnerHasOrcidId',
-      'hasOrcidId',
-      'profile'
-    ]),
+    ...mapGetters(['datasetOwnerHasOrcidId', 'hasOrcidId', 'profile']),
 
-    ...mapState([
-      'config',
-      'activeOrganization'
-    ]),
+    ...mapState(['config', 'activeOrganization']),
 
     /**
      * Compute active organization id
      * @returns {String}
      */
     activeOrganizationId: function() {
-      return pathOr('Organization', ['organization', 'id'], this.activeOrganization)
+      return pathOr(
+        'Organization',
+        ['organization', 'id'],
+        this.activeOrganization
+      )
     },
     /**
      * Retrieves Url to display with name once ORCID is connected to user profile
@@ -127,13 +110,11 @@ export default {
       return env === 'dev'
         ? `https://sandbox.orcid.org/${this.profile.orcid.orcid}`
         : `https://orcid.org/${this.profile.orcid.orcid}`
-    },
+    }
   },
 
   methods: {
-     ...mapActions([
-      'updateProfile'
-    ]),
+    ...mapActions(['updateProfile']),
 
     /**
      * Function that's called after ORCID is deleted
@@ -142,7 +123,7 @@ export default {
       this.updateProfile({
         ...this.profile,
         orcid: {}
-        })
+      })
     },
 
     /**
@@ -158,16 +139,15 @@ export default {
      */
     closeOrcidDialog: function() {
       this.deleteOrcidDialogVisible = false
-    },
-  },
+    }
+  }
 }
 </script>
-
 
 <style lang="scss" scoped>
 @import '../../../../assets/_variables.scss';
 
- .orcid-success {
+.orcid-success {
   border: solid 1px #dadada;
   padding: 10px;
   display: flex;
@@ -185,7 +165,6 @@ export default {
     padding-bottom: 20px;
     flex: 1;
   }
-
 
   .orcid-success-info {
     .orcid-success-text {
@@ -231,5 +210,4 @@ export default {
 p {
   color: black;
 }
-
 </style>
