933b5fb4c7d2020db17e9540eeeba890b933e162
Annalee Switek <annaleeswitek@brb-12331.apn.wlan.private.upenn.edu>
2023-11-06 16:33:58 -0500

    update FE to show NIH users their custom terms of use

--- src/components/bf-navigation/UserMenu/UserMenu.vue
+++ src/components/bf-navigation/UserMenu/UserMenu.vue
@@ -19,10 +19,7 @@
       >
         <div class="bf-menu scroll-menu">
           <ul>
-            <li
-              v-for="org in filteredOrganizations"
-              :key="org.organization.id"
-            >
+            <li v-for="org in filteredOrganizations" :key="org.organization.id">
               <a
                 href="#"
                 class="bf-menu-item"
@@ -38,13 +35,12 @@
                 />
               </a>
             </li>
-            <hr>
+            <hr />
             <li>
               <a
                 href="#"
                 class="bf-menu-item"
-                :class="[maxOrgsCreated ? 'disabled': '']"
-
+                :class="[maxOrgsCreated ? 'disabled' : '']"
                 @click.prevent="requestCreateOrganization"
               >
                 Request to Create Private Workspace
@@ -90,6 +86,15 @@
           <ul>
             <li>
               <a
+                v-if="activeOrganizationId === 'add-nih-org-id-here'"
+                class="bf-menu-item"
+                href="https://docs.pennsieve.io/page/nih-pennsieve-terms-of-use"
+                target="_blank"
+              >
+                NIH Terms of Use
+              </a>
+              <a
+                v-else
                 class="bf-menu-item"
                 href="https://docs.pennsieve.io/page/pennsieve-terms-of-use"
                 target="_blank"
@@ -108,19 +113,21 @@
             </li>
           </ul>
 
-          <hr>
+          <hr />
 
           <ul>
             <li>
               <router-link
                 class="bf-menu-item"
-                :to="{ name: 'my-settings-container', params: {orgId: activeOrganizationId} }"
+                :to="{
+                  name: 'my-settings-container',
+                  params: { orgId: activeOrganizationId }
+                }"
                 @click.native="closeMenus"
               >
                 View My Profile
               </router-link>
             </li>
-
 
             <li>
               <a
@@ -140,24 +147,17 @@
                 @mouseenter="onOrgMenuMouseenter"
                 @mouseleave="onOrgMenuMouseleave"
               >
-                Switch to Private Workspace <svg-icon
-                  icon="icon-arrow-right"
-                  width="10"
-                  height="10"
-                />
+                Switch to Private Workspace
+                <svg-icon icon="icon-arrow-right" width="10" height="10" />
               </a>
             </li>
           </ul>
 
-          <hr>
+          <hr />
 
           <ul>
             <li>
-              <a
-                class="bf-menu-item"
-                href="#"
-                @click.prevent="onSignOutClick"
-              >
+              <a class="bf-menu-item" href="#" @click.prevent="onSignOutClick">
                 Sign Out
               </a>
             </li>
@@ -170,7 +170,7 @@
       v-if="!pageNotFound"
       v-popover:userMenu
       class="user-menu bf-navigation-item"
-      :class="{ active : menuOpen }"
+      :class="{ active: menuOpen }"
       @focus="onUserMenuMouseenter"
       @blur="onUserMenuMouseleave"
       @mouseenter="onUserMenuMouseenter"
@@ -179,12 +179,8 @@
       <avatar />
 
       <div class="info">
-        <span id="user-name">
-          {{ displayName }}
-        </span>
-        <span id="organization-name">
-          {{ organizationName }}
-        </span>
+        <span id="user-name">{{ displayName }}</span>
+        <span id="organization-name">{{ organizationName }}</span>
         <span v-if="isWorkspaceGuest" id="workspace-guest">
           (workspace guest)
         </span>
@@ -200,18 +196,10 @@
         width="10"
       />
     </button>
-    <button
-      v-else
-      class="user-menu"
-    >
+    <button v-else class="user-menu">
       <a href="https://app.pennsieve.io">
         <div class="person-circle">
-          <svg-icon
-            icon="icon-person"
-            color="#fff"
-            width="52"
-            height="20"
-          />
+          <svg-icon icon="icon-person" color="#fff" width="52" height="20" />
         </div>
       </a>
     </button>
@@ -256,11 +244,7 @@ export default {
   },
 
   computed: {
-    ...mapState([
-      'profile',
-      'activeOrganization',
-      'organizations'
-    ]),
+    ...mapState(['profile', 'activeOrganization', 'organizations']),
 
     /**
      * Checks where user has created the maximum number of organizations
@@ -269,24 +253,36 @@ export default {
     maxOrgsCreated: function() {
       // NOTE: Adding the first condition so that this can go through
       // as these properties do not exist in the profile object yet
-      return this.profile.maxOrganizationsAllowed === 3 && this.profile.organizationsCreated === this.profile.maxOrganizationsAllowed
+      return (
+        this.profile.maxOrganizationsAllowed === 3 &&
+        this.profile.organizationsCreated ===
+          this.profile.maxOrganizationsAllowed
+      )
     },
 
     /**
-       * Checks if route is a 404 page
-       * @returns {Boolean}
-       */
-      pageNotFound: function() {
-        return this.$route.name === 'page-not-found'
-      },
+     * Checks if route is a 404 page
+     * @returns {Boolean}
+     */
+    pageNotFound: function() {
+      return this.$route.name === 'page-not-found'
+    },
 
     /**
-       * Compute active organization id
-       * @returns {String}
-       */
-      activeOrganizationId: function() {
-        return pathOr('Organization', ['organization', 'id'], this.activeOrganization)
-      },
+     * Compute active organization id
+     * @returns {String}
+     */
+    activeOrganizationId: function() {
+      console.log(
+        'i ran',
+        pathOr('Organization', ['organization', 'id'], this.activeOrganization)
+      )
+      return pathOr(
+        'Organization',
+        ['organization', 'id'],
+        this.activeOrganization
+      )
+    },
 
     /**
      * Compute organizations based off of filter
@@ -294,15 +290,18 @@ export default {
      */
     filteredOrganizations: function() {
       if (this.organizations.length) {
-
         // Update the org menu's position when the list changes
         this.$nextTick(() => {
           this.$refs.orgMenu.updatePopper()
         })
 
         return this.organizations.filter(organization => {
-          return organization.organization.name.toLowerCase().indexOf(this.orgFilterName.toLowerCase()) > -1 &&
-            organization.organization.name != "Welcome"
+          return (
+            organization.organization.name
+              .toLowerCase()
+              .indexOf(this.orgFilterName.toLowerCase()) > -1 &&
+            organization.organization.name != 'Welcome'
+          )
         })
       }
 
@@ -310,11 +309,10 @@ export default {
     },
     welcomeOrganization: function() {
       if (this.organizations.length) {
-        let welcomeOrgs =  this.organizations.filter(org => {
+        let welcomeOrgs = this.organizations.filter(org => {
           return org.organization.name == 'Welcome'
         })
         return welcomeOrgs[0]
-
       }
     },
 
@@ -325,7 +323,7 @@ export default {
     displayName: function() {
       const firstName = propOr('', 'firstName', this.profile)
       const lastName = propOr('', 'lastName', this.profile)
-      return `${firstName} ${lastName}`;
+      return `${firstName} ${lastName}`
     },
 
     /**
@@ -351,9 +349,7 @@ export default {
   },
 
   methods: {
-    ...mapActions([
-      'toggleDatasetVis'
-    ]),
+    ...mapActions(['toggleDatasetVis']),
 
     /**
      * Open Create Organization Dialog
@@ -375,7 +371,7 @@ export default {
      * Sets the dataset name visibility flag to false
      */
     setDatasetVis: function() {
-      console.log("SETTING VISIBILITY TO FALSE")
+      console.log('SETTING VISIBILITY TO FALSE')
       this.toggleDatasetVis(false)
     },
     /**
@@ -476,8 +472,8 @@ export default {
       if (!this.pageNotFound) {
         clearTimeout(this._timerUserMenu)
         this._timerUserMenu = setTimeout(() => {
-        this.menuOpen = true
-      }, 200)
+          this.menuOpen = true
+        }, 200)
       }
     },
 
@@ -506,90 +502,90 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '../../../assets/_variables.scss';
+@import '../../../assets/_variables.scss';
 
-  .person-circle {
-    margin-left: 11px;
-    position: relative;
-    align-items: center;
-    background: transparent;
-    border: solid 2px #fff;
-    border-radius: 50%;
-    box-sizing: border-box;
-    color: $white;
-    display: inline-flex;
-    font-weight: 600;
-    height: 32px;
+.person-circle {
+  margin-left: 11px;
+  position: relative;
+  align-items: center;
+  background: transparent;
+  border: solid 2px #fff;
+  border-radius: 50%;
+  box-sizing: border-box;
+  color: $white;
+  display: inline-flex;
+  font-weight: 600;
+  height: 32px;
+  overflow: hidden;
+  width: 32px;
+}
+.user-menu-wrap {
+  background: $purple_3;
+  .secondary & {
+    background: $gray_2;
+  }
+}
+.user-menu {
+  cursor: pointer;
+  height: 74px;
+  &:disabled {
+    cursor: default;
+    pointer-events: none;
+  }
+}
+.avatar-circle {
+  border: 2px solid #fff;
+  flex-shrink: 0;
+  height: 32px;
+  width: 32px;
+  .condensed & {
+    margin-left: -12px;
+  }
+}
+#user-name {
+  font-weight: 500;
+}
+#organization-name {
+  font-size: 12px;
+}
+#workspace-guest {
+  font-size: 10px;
+  color: #999999;
+}
+.icon-caret {
+  flex-shrink: none;
+}
+.info {
+  flex: 1;
+  margin: 0 8px 0 12px;
+  overflow: hidden;
+  white-space: nowrap;
+  .condensed & {
+    margin-left: 16px;
+  }
+  span {
+    display: block;
     overflow: hidden;
-    width: 32px;
+    text-overflow: ellipsis;
   }
-  .user-menu-wrap {
-    background: $purple_3;
-    .secondary & {
-      background: $gray_2;
-    }
-  }
-  .user-menu {
-    cursor: pointer;
-    height: 74px;
-    &:disabled {
-      cursor: default;
-      pointer-events: none;
-    }
-  }
-  .avatar-circle {
-    border: 2px solid #fff;
-    flex-shrink: 0;
-    height: 32px;
-    width: 32px;
-    .condensed & {
-      margin-left: -12px
-    }
-  }
-  #user-name {
-    font-weight: 500;
-  }
-  #organization-name {
-    font-size: 12px
-  }
-  #workspace-guest {
-    font-size: 10px;
-    color: #999999;
-  }
-  .icon-caret {
-    flex-shrink: none;
-  }
-  .info {
-    flex: 1;
-    margin: 0 8px 0 12px;
-    overflow: hidden;
-    white-space: nowrap;
-    .condensed & {
-      margin-left: 16px;
-    }
-    span {
-      display: block;
-      overflow: hidden;
-      text-overflow: ellipsis;
-    }
-  }
-  .filter-input {
-    border-bottom: none;
-    border-top: 1px solid $gray_2;
-  }
+}
+.filter-input {
+  border-bottom: none;
+  border-top: 1px solid $gray_2;
+}
 </style>
 <style lang="scss">
-  @import '../../../assets/_variables.scss';
+@import '../../../assets/_variables.scss';
 
-  .user-menu-org-menu {
-    margin-left: 0 !important;
-  }
-  .user-menu-popover {
-    margin-left: 16px !important;
-  }
-  .icon-check {
-    margin-top: -4px;
-    float: right;
-    color: $gray_4;
-  }
+.user-menu-org-menu {
+  margin-left: 0 !important;
+}
+.user-menu-popover {
+  margin-left: 16px !important;
+}
+.icon-check {
+  margin-top: -4px;
+  float: right;
+  color: $gray_4;
+}
 </style>
